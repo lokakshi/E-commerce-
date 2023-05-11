462503d3b740fdd0c0e0aafc97d116c60145edb5
adityalokakshijee@gmail.com <adityalokakshi@Adityas-MacBook-Air.local>
2023-05-11 16:28:56 +0530

    git initialization

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+function App() {
+  return (
+  <>
+  <Header></Header>
+  <div></div>
+  <Footer></Footer>
+  </>
+  );
+}
+
+export default App;
