f9be2a90d811a57104aa20006d292c88f6da0fd5
adityalokakshijee@gmail.com <adityalokakshi@Adityas-MacBook-Air.local>
2023-05-11 22:28:46 +0530

    Adding rating component and UI changes to products page

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-
+import Rating from '../Components/Rating'
 const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
@@ -12,9 +12,9 @@ const Product = ({ product }) => {
                     <Card.Title as='div'> <strong>{product.name}</strong></Card.Title>
                 </a>
                 <Card.Text as='div'>
-                    <div className='my-3'>
-                        {product.rating} from {product.numReviews} reviews
-                    </div>
+                    <Rating 
+                        value={product.rating} text={`${product.numReviews} reviews`}>
+                    </Rating>
                 </Card.Text>
                 <Card.Text as='h3'>${product.price}</Card.Text>
             </Card.Body>
